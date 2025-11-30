13d18ff3721fd0bc777ec6499856006c13226f32
tmg1981 <moradi6021@gmail.com>
2025-11-30 21:54:04 +0330

    update site title to AffiliateFlow

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,10 +1,10 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  title: 'AffiliateFlow – AI Affiliate Post Generator',
+  author: 'tmg1981',
+  headerTitle: 'AffiliateFlow',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
-  theme: 'system', // system, dark or light
+  theme: 'dark', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
